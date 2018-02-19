5b3eea6cc19463af37494a92820b2c78cc714258
Pedro Bautista <pedro.bautistamx@gmail.com>
2018-02-19 16:06:32 -0600

    Segundo Coment

---
+++ src/myapp/core.clj
@@ -0,0 +1,35 @@
+(ns myapp.core
+  (:gen-class))
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
+
+
+(defn fibonacci [x] ;;Funcion recursiva para serie de Fibonacci
+(if (<= x 2) 1 ;;los dos primeros elementos dela serie siempre son "1"
+    (+ (fibonacci (- x 2)) (fibonacci (- x 1))))) ;; Se suma 
+
+
+
+(defn factorial [x]
+
+
+(if (<= x 1) 1
+    (* (factorial (- x 1)) x)))
+
+
+
+
+
+
+
+(defn -main [& args]
+
+  (let [arg-1 (or (first args) "0")
+n (Integer/parseInt arg-1)]
+
+(println "fib(" n ") = " (fibonacci n))
+(println "!(" n ") = " (factorial n))))
+	;; (foo "main!!"))
